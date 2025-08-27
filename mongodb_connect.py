728690a544f9a0b152dcf8ddb4c27803639a5545
Ishar786 <isharsheik33863@gmail.com>
2025-08-27 21:42:44 +0530

    Update mongodb_connect.py

--- mongodb_connect.py
+++ mongodb_connect.py
@@ -1,15 +1,1 @@
 
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-
-uri = "mongodb+srv://isharsheik33863:XhZoUEN4v3othw3D@cluster0.xzsexnt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
